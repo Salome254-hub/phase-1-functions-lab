abeed0384fceddfe8ec4be4e990be07223cd4370
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-07 19:46:54 +0300

    commit tree 5079710d4554183b090f40ffac0fd53fc84112f1

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks)
+ {
+    if(blocks>42){
+    
+        console.log(blocks=blocks-42);
+        return blocks;
+    
+    }
+    
+    else
+    {console.log(blocks=42-blocks);
+      return blocks;
+    }
+    
+    
+}
+
+const dfeet=function distanceFromHqInFeet(feet){
+    
+    
+distanceFromHqInBlocks(blocks);  
+if (feet>264) {
+    console.log(feet=blocks*264);
+    return feet;
+    }
+else {feet<264
+    console.log(feet=blocks*264);
+    return feet;
+
+}
+
+
+}
+    
+function distanceTravelledInFeet(start, destination,distance){
+    
+    console.log(distance=(start-destination)*264);
+    return distance;
+
+
+
+}
+function calculatesFarePrice(distance,price){
+   
+if(distance<400){
+console.log(price=o);
+return price;
+}
+else if(distance>400&&distance<2000)
+{
+    console.log(price=price*2)
+    return price;
+
+}
+else if(distance>2000&&distance<2500){
+
+    console.log(price=price*25)
+    return price;
+}
+else {
+    console.log(distance>2500)
+    
+    return 'cannot travel that far';
+
+}
+
+}
+  
